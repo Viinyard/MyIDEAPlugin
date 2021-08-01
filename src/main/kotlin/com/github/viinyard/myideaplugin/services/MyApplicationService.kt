3de0ee16e9244cf949df87eb92a8576581bbb93d
GitHub Action <action@github.com>
2021-08-01 16:53:51 +0000

    Template cleanup

--- src/main/kotlin/com/github/viinyard/myideaplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/viinyard/myideaplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.viinyard.myideaplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.viinyard.myideaplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/viinyard/myideaplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/viinyard/myideaplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.viinyard.myideaplugin.services
 
+import com.github.viinyard.myideaplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
